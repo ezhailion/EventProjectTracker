1b1024d276e2c91a61cb8dad3f7c95a1132760e3
Sawyer Harnedy <sawyer@brucebs-mbp-2.attlocal.net>
2023-10-06 16:56:34 -0600

    created boot project, add jpa dependencies

---
+++ EsportsTracker/src/main/java/com/skilldistillery/esports/EsportsTrackerApplication.java
@@ -0,0 +1,13 @@
+package com.skilldistillery.esports;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EsportsTrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EsportsTrackerApplication.class, args);
+	}
+
+}
---
+++ EsportsTracker/src/test/java/com/skilldistillery/esports/EsportsTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.skilldistillery.esports;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EsportsTrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
